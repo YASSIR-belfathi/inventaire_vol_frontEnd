d60e73e1eea361dc2aacd1cce982a4bcf912fbd7
YASSIR BELFATHI <belfathiy@gmail.com>
2024-12-10 00:59:55 +0100

    add the home page of website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./home_page/components/header.js";
+import Main from "./home_page/components/main.js";
+import Cards from "./home_page/components/Cards.js";
+import Footer from "./home_page/components/Footer.js";
+import "./Style.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Cards />
+      <Footer />
     </div>
   );
 }
---
+++ src/home_page/components/Footer.js
@@ -0,0 +1,27 @@
+import "../style/Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="containerFooter">
+      <div className="titleFooter">
+        <p>
+          <i>Besoin? Contactez nous</i>
+        </p>
+      </div>
+      <div className="containerBlock blockElement">
+        <div className="containerEmail">
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="containerMessage">
+          <textarea placeholder="Message" />
+        </div>
+        <div className="containerSubmit">
+          <button type="submit">Envoyer</button>
+        </div>
+      </div>
+      <div className="containerCopyRight">
+        <i>&copy;CopyRight 2024. All Right Reserved.</i>
+      </div>
+    </div>
+  );
+}
